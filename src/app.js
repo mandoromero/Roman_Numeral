68cdabc8b618f9e1261a56e3d6e3d943d7b8c363
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2025-01-16 16:19:01 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,68 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.getElementById('convert-btn').addEventListener('click', function() {
+  const numberInput = document.getElementById('number').value.trim();
+  const outputCon = document.getElementById('output-con');
+  const output = document.getElementById('output');
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  if (numberInput === '') {
+    output.textContent = "Please enter a valid number";
+    output.style.fontSize = "20px";
+    return;
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const parsedNumber = parseInt(numberInput);
+
+  if (isNaN(parsedNumber) || parsedNumber < 1) {
+    output.textContent = "Please enter a number greater than or equal to 1";
+    output.style.fontSize = "20px";
+    return;
+  }
+  
+  if (parsedNumber >= 4000) {
+    output.textContent = "Please enter a number less than or equal to 3999";
+    output.style.fontSize = "20px";
+    return;
+  }
+
+  const romanNumeral = convertToRoman(parsedNumber);
+  output.textContent = `${romanNumeral}`;
+  output.style.fontSize = "45px"
+  
+  outputCon.style.display = 'block';
+});
+
+document.getElementById('clear-btn').addEventListener('click', function() {
+  const numberInput = document.getElementById('number');
+  const output = document.getElementById('output');
+  const outputCon = document.getElementById('output-con');
+  
+  numberInput.value = "";
+  output.value = "";
+  outputCon.style.display = "none";
+})
+
+function convertToRoman(num) {
+  const romanLookup = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+  
+  let roman = '';
+  for (let key in romanLookup) {
+    while (num >= romanLookup[key]) {
+      roman += key;
+      num -= romanLookup[key];
+    }
+  }
+  return roman;
+}
